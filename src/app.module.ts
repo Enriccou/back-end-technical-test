2e6f56511ec856ab4b241e7be56104925748b6dd
Enriccou <enriccowhz@gmail.com>
2025-10-05 09:27:08 -0300

    Fix: user access folder name

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AccessModule } from './access/access.module';
+import { AccessModule } from './userAccess/access.module';
 
 @Module({
   imports: [
