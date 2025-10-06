2cc7535bb4566e31e0c82579ee1823bcdeb8fad8
Enriccou <enriccowhz@gmail.com>
2025-10-06 14:55:49 -0300

    Feat: Environment variables And Vercel Json

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,18 @@ import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AccessModule } from './userAccess/access.module';
 import { SiteAccessModule } from './siteAccess/site-access.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://enriccou:whz2025dev@cluster0.3mw4lbl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-    ),
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (ConfigService: ConfigService) => ({
+        uri: ConfigService.get<string>('DATABASE_URL'),
+      }),
+      inject: [ConfigService],
+    }),
     UsersModule,
     AccessModule,
     SiteAccessModule,
