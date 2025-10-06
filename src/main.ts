0b119561065a3873e0e6ca07afede1dae8f4ec5c
Enriccou <enriccowhz@gmail.com>
2025-10-06 15:35:12 -0300

    Fix: Vercel upload

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as swaggerUi from 'swagger-ui-express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,11 +16,13 @@ async function bootstrap() {
   const config = new DocumentBuilder()
     .setTitle('Test for internal junior BackEnd position')
     .setDescription('Desenvolvido por Enricco Bertazzo')
-    .setVersion('')
+    .setVersion('1.0.3')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('', app, document);
+  SwaggerModule.setup('api', app, document);
+
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(document));
 
   await app.listen(process.env.PORT ?? 3000);
 }
